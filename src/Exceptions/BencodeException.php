8241e9db103e3cbb0f3bae91ffa251a29c5b5b14
pxgamer <owzie123@gmail.com>
2018-12-17 23:17:24 +0000

    Add visibility to exception constants

--- src/Exceptions/BencodeException.php
+++ src/Exceptions/BencodeException.php
@@ -2,20 +2,23 @@
 
 namespace pxgamer\Torrent\Exceptions;
 
+/**
+ * Class BencodeException
+ */
 class BencodeException extends \Exception
 {
-    const STRING_LEADING_ZERO = 'Invalid string length, leading zero.';
-    const STRING_COLON_NOT_FOUND = 'Invalid string length, colon not found.';
-    const STRING_INPUT_TOO_SHORT = 'Invalid string, input too short for string length.';
+    public const STRING_LEADING_ZERO = 'Invalid string length, leading zero.';
+    public const STRING_COLON_NOT_FOUND = 'Invalid string length, colon not found.';
+    public const STRING_INPUT_TOO_SHORT = 'Invalid string, input too short for string length.';
 
-    const INT_IS_EMPTY = 'Empty integer.';
-    const INT_LEADING_ZERO = 'Leading zero in integer.';
-    const INT_NON_DIGIT_CHARS = 'Non-digit characters in integer.';
+    public const INT_IS_EMPTY = 'Empty integer.';
+    public const INT_LEADING_ZERO = 'Leading zero in integer.';
+    public const INT_NON_DIGIT_CHARS = 'Non-digit characters in integer.';
 
-    const LIST_UNTERMINATED = 'Unterminated list.';
+    public const LIST_UNTERMINATED = 'Unterminated list.';
 
-    const DICTIONARY_MIS_SORTED_KEYS = 'Mis-sorted dictionary key.';
-    const DICTIONARY_DUPLICATE_KEY = 'Duplicate dictionary key.';
-    const DICTIONARY_INVALID_KEY = 'Invalid dictionary key.';
-    const DICTIONARY_UNTERMINATED = 'Unterminated dictionary.';
+    public const DICTIONARY_MIS_SORTED_KEYS = 'Mis-sorted dictionary key.';
+    public const DICTIONARY_DUPLICATE_KEY = 'Duplicate dictionary key.';
+    public const DICTIONARY_INVALID_KEY = 'Invalid dictionary key.';
+    public const DICTIONARY_UNTERMINATED = 'Unterminated dictionary.';
 }
